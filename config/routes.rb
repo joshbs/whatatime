67ff532f45d4b49c43535f5a4359d73fbc2ab990
Noah H. Smith <nsmith@rendergardenmedia.com>
2011-09-23 10:43:55 -0500

    Removed start & stop actions from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Whatatime::Application.routes.draw do
-  put '/time_entries/:id/start' => 'time_entries#start', :as => :start_time_entry
-  put '/time_entries/:id/stop' => 'time_entries#stop', :as => :stop_time_entry
   resources :time_entries
 
   root :to => 'time_entries#index'
