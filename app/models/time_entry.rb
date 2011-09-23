ded66675eb35415c100aff76890432eb494e3f8c
shua <shua@rendergardenmedia.com>
2011-09-23 11:24:54 -0500

    added 'to_relative_time' to the time_entry model. this feature sends a text notice stating how long ago the entry was started at, stopped at, or if it has not been activated at all

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,4 +1,5 @@
 class TimeEntry < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
 
   def timer_running?
     !self.started_at.nil? && !self.timer_complete?
@@ -7,5 +8,12 @@ class TimeEntry < ActiveRecord::Base
   def timer_complete?
     !self.stopped_at.nil?
   end
-
+  
+  def to_relative_time
+    if self.timer_running?
+      "started #{distance_of_time_in_words_to_now(self.started_at, true)} ago"
+    elsif self.timer_complete?
+      "stopped #{distance_of_time_in_words_to_now(self.stopped_at, true)} ago"
+    end
+  end
 end
