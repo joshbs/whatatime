e1914703820b625c8ea4710467aa5088f3ef50d4
shua <shua@rendergardenmedia.com>
2011-09-23 14:26:24 -0500

    added login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Whatatime::Application.routes.draw do
   devise_for :users
 
+  as :user do
+    get "/login" => "devise/sessions#new"
+    get "/logout" => "devise/sessions#destroy"
+  end
+
   resources :time_entries
 
   root :to => 'time_entries#index'
