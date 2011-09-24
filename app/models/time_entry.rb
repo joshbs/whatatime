664db17d65679cc7791485bd4fb5edea395ef1ba
Noah H. Smith <nsmith@rendergardenmedia.com>
2011-09-24 15:28:14 -0500

    Now using time_ago_in_words, because it's shorter. Shorter = cooler.

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -11,9 +11,9 @@ class TimeEntry < ActiveRecord::Base
   
   def to_relative_time
     if self.timer_running?
-      "started #{distance_of_time_in_words_to_now(self.started_at, true)} ago"
+      "started #{time_ago_in_words(self.started_at, true)} ago"
     elsif self.timer_complete?
-      "stopped #{distance_of_time_in_words_to_now(self.stopped_at, true)} ago"
+      "stopped #{time_ago_in_words(self.stopped_at, true)} ago"
     end
   end
 end
